9a61487c750d897479912ca85e7caade53ad7ae5
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2023-09-26 14:02:55 +0300

    add new button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const createNewAccount = () => {
+    const wallet = ethers.Wallet.createRandom();
+    setAccount({
+      address: wallet.address,
+      privateKey: wallet.privateKey
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={createNewAccount}
+      >
+        New Account
+      </button>
+      {account && (
+        <div className="mt-4 text-center">
+          <h2 className="text-xl font-bold">Generated Account</h2>
+          <p>Address: {account.address}</p>
+          <p>Private Key: {account.privateKey}</p>
+        </div>
+      )}
     </div>
   );
 }
